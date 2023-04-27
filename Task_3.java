748fddde068308509cbbfa4e2ed269f58fd9d788
«Kristina» <kristinka007_92@mail.ru>
2023-04-27 14:38:36 +0300

    Задача 3

---
+++ Task_3.java
@@ -0,0 +1,19 @@
+//Найдите сумму всех элементов LinkedList, сохраняя все элементы в списке. Используйте итератор
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.LinkedList;
+
+public class Task_3 {
+    public static void main(String[] args) {
+        LinkedList<Integer> myLL = new LinkedList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19));
+        Iterator<Integer> iterator = myLL.iterator();
+
+        int sum = 0;
+        while (iterator.hasNext()){
+            sum += iterator.next();
+        }
+
+        System.out.println(sum);
+    }
+}
